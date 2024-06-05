adb11618bd1677f60a425384275ce65b39419245
Alessandro Campo <alessandrocampo97@gmail.com>
2024-06-05 16:40:00 +0200

    feat: createPost function

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('app.js running');
+const { createPost } = require("./services/PostCrud");
+
+const testPost = {
+    name: 'Test Post',
+    content: 'Some random text as example description',
+    published: false,
+    category: 'Technology',
+    tags: ['Entertainment', 'Business', 'Finance']
+}
+
+
+createPost(testPost);
---
+++ services/PostCrud.js
@@ -0,0 +1,47 @@
+const { PrismaClient } = require('@prisma/client');
+const slugify = require('slugify');
+const prisma = new PrismaClient();
+
+const createPost = async function (post) {
+    const categories = await prisma.category.findMany();
+    const tags = await prisma.tag.findMany();
+    console.log(categories)
+    const foundCategory = categories.find(c => c.name.toLocaleLowerCase() === post.category.toLocaleLowerCase()).id;
+    const foundTags = post.tags.reduce((acc, t) => {
+        const foundTag = tags.find(tag => tag.name.toLocaleLowerCase() === t.toLocaleLowerCase())
+        if (foundTag) {
+            return [...acc, { id: foundTag.id }]
+        }
+        return acc
+    }, [])
+    if (!foundCategory) {
+        throw new Error(`There is no category with name ${post.category}`)
+    }
+    if (foundTags.length === 0) {
+        throw new Error(`None of the provided tags exists in the tag table`)
+    }
+
+    try {
+        const newPost = await prisma.post.create({
+            data: {
+                name: post.name,
+                slug: slugify(post.name),
+                image: post.image || null,
+                content: post.content,
+                categoryId: foundCategory,
+                published: post.published,
+                tags: {
+                    connect: foundTags
+                }
+
+            }
+        })
+        return newPost
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = {
+    createPost
+}
